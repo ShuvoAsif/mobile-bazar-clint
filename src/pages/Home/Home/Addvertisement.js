12c654e5ad181047700ba5ed5176f50229947824
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-11-27 22:56:24 +0600

    advertise

--- src/pages/Home/Home/Addvertisement.js
+++ src/pages/Home/Home/Addvertisement.js
@@ -13,9 +13,15 @@ const Addvertisement = () => {
     }, [])
 
 
+
     return (
         <div className='bg-zinc-100 pb-3'>
-            <h1 className="text-4xl py-5 text-center font-bold">Advertised Items</h1>
+            {
+                addmobiles ?
+                    <h1 className="text-4xl py-5 text-center font-bold">Advertised Items</h1>
+                    :
+                    <></>
+            }
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
                 {
                     addmobiles.map(addmobile => <AddCard
@@ -24,7 +30,7 @@ const Addvertisement = () => {
                     ></AddCard>)
                 }
             </div>
-        </div>
+        </div >
     );
 };
 
